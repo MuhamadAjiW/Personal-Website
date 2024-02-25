b209f92c066f813f3e5708f998d8db34df050407
MuhamadAjiW <13521095@std.stei.itb.ac.id>
2024-02-25 21:15:32 +0700

    fix: startup script

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 echo "Starting database..."
 docker-compose up --build -d
 echo "Starting backend..."
-cd personal-backend/dist && nvm use 20.10.0 && node index.js >> be-app.log 2>&1 &
+cd personal-backend/dist && node index.js >> be-app.log 2>&1 &
 echo "Initialization complete, check respective logs for status."
