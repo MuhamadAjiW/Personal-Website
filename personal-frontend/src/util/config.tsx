b3281ba2f9dade433d3456202ad357c7c3be26be
Muhamad Aji Wibisono <muhamadajiw71@gmail.com>
2024-11-20 16:45:12 +0000

    fix: vulnerabilities, configuration

--- personal-frontend/src/util/config.tsx
+++ personal-frontend/src/util/config.tsx
@@ -1,5 +1,5 @@
 
 export class Config {
-    static BE_URI: string="http://localhost:5174";
-    static GA_CODE: string="G-98HW834Y93";
+    static BE_URI: string = "http://localhost:50002";
+    static GA_CODE: string = "G-98HW834Y93";
 }
