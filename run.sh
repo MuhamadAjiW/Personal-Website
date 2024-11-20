ef57d084819cd5d633b072807875bf966e85e83e
Muhamad Aji Wibisono <muhamadajiw71@gmail.com>
2024-11-20 16:52:28 +0000

    chore: removed deprecated scripts, configured vite

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Starting database..."
-docker-compose up --build -d
-echo "Starting backend..."
-cd personal-backend/dist && node index.js >> be-app.log 2>&1 &
-echo "Initialization complete, check respective logs for status."
